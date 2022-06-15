469cca4e4daa5f8880e5c4063429a6b665cc156c
Alejandro Waldo <alejandro.waldo5978@alumnos.udg.mx>
2022-06-15 06:40:32 -0500

    2da oportunidad

--- ejercicio1.php
+++ ejercicio1.php
@@ -2,59 +2,93 @@
     echo "Ejercicio 1";
     echo "<br>";
 
+    $ctr = $_GET["caracteres"];
+    $caracteres = explode(" ", $ctr);
+    $instrucciones = $_GET["instrucciones"];
+    $entradas = explode(" ", $instrucciones);
+    $trasmisor = $_GET["trasmisor"];
+    $buscado = $trasmisor;
+
     /* Obtenemos los datos del archivo */
+    /*
     $fp = fopen("miarchivo.txt", "r");
     $band = 0;
     $caracteres = [];
     $entradas = [];
+    $buscado = "";
     while (!feof($fp)){
         $linea = fgets($fp);
         if($band == 0) {
            $caracteres = explode(" ", $linea);
-           $band = 1;
-           
-        } else {
+        } else if($band == 3) {
+            $buscado = $linea;
+        }
+        else {
             $entradas[] = $linea;
         }
+        $band++;
     }
     fclose($fp);
+    */
 
     /* Fin de lectura de los datos del archivo */
-    
-    $busqueda1 = $entradas[0];
-    $busqueda2 = $entradas[1];
-    $buscado = $entradas[2];
-    
-    
-    $coincidencias1 = 0;
-    $coincidencias2 = 0;
-    for($i = 0; $i < (int)$caracteres[0]; $i++){
-        for($j = 0; $j < $caracteres[2]; $j++) {
-            if($busqueda1[$i] == $buscado[$j]) {
-                $coincidencias1++;
-                break;
-            }
+
+    /*Limpiar cadena*/
+    $buscado_sin = "";
+    for ($i = 0; $i < strlen($buscado); $i++) {
+        if($buscado_sin[strlen($buscado_sin) - 1] == $buscado[$i]) {
+        } else {
+            $buscado_sin .= $buscado[$i];
         }
     }
+    
+    /*Fin Limpiar cadena*/
 
-    for($i = 0; $i < (int)$caracteres[1]; $i++){
-        for($j = 0; $j < $caracteres[2]; $j++) {
-            if($busqueda2[$i] == $buscado[$j]) {
-                $coincidencias2++;
-                break;
+    /* Buscar mensaje encriptado */
+    $enc1 = 0;
+    $enc2 = 0;
+    for ($i = 0; $i < strlen($buscado_sin); $i++) {
+        if($entradas[0][0] == $buscado_sin[$i]) {
+            $resultado = substr($buscado_sin, $i, strlen($entradas[0]) - 1);
+            $comparacion = substr($entradas[0], 0, strlen($entradas[0]) - 1);
+            if($resultado == $comparacion) {
+                echo "SI" . "<br>";
+                $enc1 = 1;
             }
         }
+
     }
 
-    if($coincidencias1 == (int)$caracteres[0]){
-        echo "SI";
-    } else {
-        echo "NO";
+    for ($i = 0; $i < strlen($buscado_sin); $i++) {
+        if($entradas[1][0] == $buscado_sin[$i]) {
+            $resultado = substr($buscado_sin, $i, strlen($entradas[1]) - 1);
+            $comparacion = substr($entradas[1], 0, strlen($entradas[1]) - 1);            
+            if($resultado == $comparacion) {
+                echo "SI" . "<br>";
+                $enc2 = 1;
+            }
+        }
+
     }
-    echo "<br>";
-    if($coincidencias2 == (int)$caracteres[1]){
-        echo "SI";
+    
+    $final = "";
+
+    if($enc1 == 0) {
+        echo "NO" . "<br>";
+        $final .= "NO \n";
     } else {
-        echo "NO";
+        $final .= "SI \n";
     }
-?>
+    if($enc2 == 0) {
+        echo "NO" . "<br>";
+        $final .= "NO";
+    } else {
+        $final .= "SI";
+    }
+
+    $file = fopen("resultado.txt", "w");
+        fputs($file,$final);
+        fclose($file);
+
+    //example: https://mail.alejandrowaldo.com/ejercicio1.php?caracteres=11%2015%2038&instrucciones=CeseAlFuego%20CorranACubierto&trasmisor=XXcaaamakkCCessseAAllFueeegooDLLKmmNNN
+    //result: https://mail.alejandrowaldo.com/resultado.txt
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,16 @@
+import sys
+
+"""
+5 140 82 89 134 90 110 112 106 88 90 
+"""
+
+
 print("Ejercicio 2 \n")
+
+
+f = open("tabla.txt","w")
+f.write(f"{sys.argv[1]}\n{sys.argv[2]} {sys.argv[3]}\n{sys.argv[4]} {sys.argv[5]}\n{sys.argv[6]} {sys.argv[7]}\n{sys.argv[8]} {sys.argv[9]}\n{sys.argv[10]} {sys.argv[11]}")
+f.close()
 
 band = 0
 cantidad_rondas = 0
@@ -6,6 +18,7 @@ rondas = []
 jugador1 = []
 jugador2 = [] 
 ganador_ronda = []
+diferencias = []
 with open("tabla.txt") as archivo:
     for linea in archivo:
         if(band == 0):
@@ -13,27 +26,31 @@ with open("tabla.txt") as archivo:
             band = 1
         else:
             rondas = linea.split(' ')
-            jugador1.append(int(rondas[0]))
-            jugador2.append(int(rondas[1]))
-            if(int(rondas[0]) > int(rondas[1])):
-                diferencia = int(rondas[0]) - int(rondas[1])
-                ganador_ronda.append([1,diferencia])
+            if(band == 1): 
+                jugador1.append(int(rondas[0]))
+                jugador2.append(int(rondas[1]))    
+                band = 2
             else:
-                diferencia = int(rondas[1]) - int(rondas[0])
-                ganador_ronda.append([2,diferencia])
+                index = len(jugador1) - 1
+                jugador1.append(int(rondas[0]) + jugador1[index])
+                jugador2.append(int(rondas[1]) + jugador2[index])
+                
 
-mayor = ganador_ronda[0][1]
-ganador = ganador_ronda[0][0]
+for i in range(cantidad_rondas):
+    if(jugador1[i] > jugador2[i]):
+        diferencia = jugador1[i] - jugador2[i]
+        ganador_ronda.append([1])
+        diferencias.append([diferencia])
+    else:
+        diferencia = jugador2[i] - jugador1[i]
+        ganador_ronda.append([2])
+        diferencias.append([diferencia])
 
-for i in range(cantidad_rondas-1):
-    dif1 = ganador_ronda[i][1]
-    dif2 = ganador_ronda[i+1][1]
-    
-    if(mayor <= dif1):
-        mayor = dif1
-        ganador = ganador_ronda[0][0]
-    elif (mayor <= dif2):     
-        mayor = dif2
-        ganador = ganador_ronda[1][0]
-    
-print(f"{ganador} {mayor}")
+
+diferencia_ganadora = max(diferencias)
+posicion = diferencias.index(diferencia_ganadora)
+ganador = ganador_ronda[posicion]
+
+f = open('resultado.txt','w')
+f.write(f"{ganador[0]} {diferencia_ganadora[0]}")
+f.close()
